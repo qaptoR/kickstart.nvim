3586305697912f1ad94ac25ef07bbea27552d49a
qaptoR <rocco.ruscitti@outlook.com>
2024-06-28 22:27:17 -0700

    adding mini.<modules> one at a time

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
-      -- Better Around/Inside textobjects
+      -- [[ Better Around/Inside textobjects ]]
       --
       -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
@@ -10,18 +10,18 @@ return {
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- [[ Add/delete/replace surroundings ]]
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
+      -- [[ Simple and easy statusline ]]
+      --
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- You can configure sections in the statusline by overriding their
@@ -32,8 +32,46 @@ return {
         return '%2l:%-2v'
       end
 
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      -- [[ Align Text Visually ]]
+      require('mini.align').setup()
+
+      -- TODO
+      -- require('mini.bufremove').setup()
+      -- require('mini.comment').setup()
+      -- require('mini.indentscope').setup()
+      -- require('mini.pairs').setup()
+
+      -- require('mini.animate').setup()
+      -- require('mini.base16').setup()
+      -- require('mini.basics').setup()
+      -- require('mini.bracketed').setup()
+      -- require('mini.clue').setup()
+      -- require('mini.colors').setup()
+      -- require('mini.completion').setup()
+      -- require('mini.cursorword').setup()
+      -- require('mini.deps').setup()
+      -- require('mini.diff').setup()
+      -- require('mini.doc').setup()
+      -- require('mini.extra').setup()
+      -- require('mini.files').setup()
+      -- require('mini.fuzzy').setup()
+      -- require('mini.git').setup()
+      -- require('mini.hipatterns').setup()
+      -- require('mini.hues').setup()
+      -- require('mini.jump').setup()
+      -- require('mini.jump2d').setup()
+      -- require('mini.map').setup()
+      -- require('mini.misc').setup()
+      -- require('mini.move').setup()
+      -- require('mini.notify').setup()
+      -- require('mini.operators').setup()
+      -- require('mini.pick').setup()
+      -- require('mini.splitjoin').setup()
+      -- require('mini.starter').setup()
+      -- require('mini.tabline').setup()
+      -- require('mini.test').setup()
+      -- require('mini.trailspace').setup()
+      -- require('mini.visits').setup()
     end,
   },
 }
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -11,7 +11,9 @@ return {
     event = 'VimEnter',
     branch = '0.1.x',
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      {
+        'nvim-lua/plenary.nvim',
+      },
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
 
@@ -25,10 +27,20 @@ return {
           return vim.fn.executable 'make' == 1
         end,
       },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      {
+        'nvim-telescope/telescope-ui-select.nvim',
+      },
+      { -- Useful for getting pretty icons, but requires a Nerd Font.
+        'nvim-tree/nvim-web-devicons',
+        enabled = vim.g.have_nerd_font,
+      },
+      {
+        'nvim-telescope/telescope-project.nvim',
+      },
+      {
+        'nvim-telescope/telescope-file-browser.nvim',
+        -- dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+      },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -72,19 +84,25 @@ return {
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'file_browser')
+      pcall(require('telescope').load_extension, 'project')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'S/earch H/elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'S/earch K/eymaps' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'S/earch F/iles' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'S/earch S/elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'S/earch current W/ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'S/earch by G/rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'S/earch D/iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'S/earch R/esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'S/earch Recent - Repeatable' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>pp', function()
+        require('telescope').extensions.project.project {}
+      end, { desc = 'P/roject P/icker' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -93,7 +111,7 @@ return {
           winblend = 10,
           previewer = false,
         })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+      end, { desc = 'Fuzzy search in current buffer' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
@@ -102,13 +120,14 @@ return {
           grep_open_files = true,
           prompt_title = 'Live Grep in Open Files',
         }
-      end, { desc = '[S]earch [/] in Open Files' })
+      end, { desc = "S/earch '/' in Open Files" })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      end, { desc = 'S/earch N/eovim files' })
     end,
   },
 }
+
 -- vim: ts=2 sts=2 sw=2 et
