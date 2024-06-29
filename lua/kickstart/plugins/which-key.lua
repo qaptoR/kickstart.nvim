48570a5e514cde4cd7cff665e9a47c64304f0719
qaptoR <rocco.ruscitti@outlook.com>
2024-06-29 12:18:42 -0700

    bug found in which-key key_labels not properly being applied

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -16,23 +16,40 @@ return {
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    opts = {
+      key_labels = { --  FIX: waiting for a fix from repo
+        ['<space>'] = "S'",
+        ['<cr>'] = "R'",
+        ['<tab>'] = "T'",
+        ['<leader>'] = "L'",
+      },
+    },
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
-      -- Document existing key chains
+      -- Document existing/core key chains
       require('which-key').register {
+        -- [[ KICKSTART ]]
         ['<leader>c'] = { name = 'C/ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = 'D/ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = 'R/ename', _ = 'which_key_ignore' },
+        ['<leader>R'] = { name = 'R/ename', _ = 'which_key_ignore' }, -- REMAP
         ['<leader>s'] = { name = 'S/earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = 'W/orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = 'T/oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git H/unk', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'H/unk, Git', _ = 'which_key_ignore' },
+
+        -- [[ PERSONAL ]]
+        ['<leader>g'] = { name = 'G/PT Tools', _ = 'which_key_ignore' },
+        ['<leader>q'] = { name = 'Q/aptor', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = 'R/occo', _ = 'which_key_ignore' },
+        ['<leader>p'] = { name = 'P/icker', _ = 'which_key_ignore' },
+        ['<leader>o'] = { name = 'O/rgmode', _ = 'which_key_ignore' },
       }
 
       -- visual mode
       require('which-key').register({
-        ['<leader>h'] = { 'Git H/unk' },
+        ['<leader>h'] = { 'H/unk, Git' },
+        ['<leader>g'] = { 'G/PT Tools' },
       }, { mode = 'v' })
     end,
   },
